1a649456e350a1f4c405442d6915b3ea261fd053
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-01-29 01:33:12 -0500

    feature and list issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -19,7 +19,7 @@ if (username) {
 
   var getUserRepos = function(user) {
     // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
   
     // make a request to the url
     fetch(apiUrl)
@@ -39,6 +39,7 @@ if (username) {
     });
   };
   var displayRepos = function(repos, searchTerm) {
+    
     // check if api returned any repos
 if (repos.length === 0) {
   repoContainerEl.textContent = "No repositories found.";
@@ -55,8 +56,14 @@ for (var i = 0; i < repos.length; i++) {
   var repoName = repos[i].owner.login + "/" + repos[i].name;
 
   // create a container for each repo
-  var repoEl = document.createElement("div");
+  var repoEl = document.createElement("a");
   repoEl.classList = "list-item flex-row justify-space-between align-center";
+  repoEl.setAttribute("href", "./single-repo.html");
+  // create a span element to hold repository name
+  // create a link for each repo
+var repoEl = document.createElement("a");
+repoEl.classList = "list-item flex-row justify-space-between align-center";
+repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
   // create a span element to hold repository name
   var titleEl = document.createElement("span");
